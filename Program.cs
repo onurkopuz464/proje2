63d8235e22cbb15ddf37e8719ebe2c8bba88c328
onurkopuz464 <onurkopuz464@gmail.com>
2022-10-22 14:01:31 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int x,y,z,toplam;
+
+
+             x = 4;
+             y = 8;
+             z = 12;
+             toplam = 16;
+             x +=2;
+             y *=2;
+             z /=4;
+
+             bool success = true;
+             bool unsuccess = x<y;
+
+             if(success && unsuccess)
+             {
+                Console.WriteLine("okey!");
+             }
+             
+             toplam = toplam%y;
+             Console.WriteLine(toplam);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
